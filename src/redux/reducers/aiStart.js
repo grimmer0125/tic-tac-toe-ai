602e42766340f90f95ce2c66b16c47ba8199544e
Grimmer <grimmer0125@gmail.com>
2018-04-15 21:58:59 +0800

    Disable automatically start the game

--- src/redux/reducers/aiStart.js
+++ src/redux/reducers/aiStart.js
@@ -3,8 +3,9 @@ import getInitialGame from '../../ai/getInitialGame';
 import { askAiMove } from '../actions';
 
 const aiStart = (oldGame) => {
+  console.log('aistart'); //3 times !!!
   setTimeout(() => store.dispatch(askAiMove()), 2000);
-  return getInitialGame(oldGame);
+  // return getInitialGame(oldGame);
 };
 
 export default aiStart;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,20 +3,24 @@ import moveAiAndNewGame from './moveAiAndNewGame';
 import moveUserAndAi from './moveUserAndAi';
 import askAiMove from './askAiMove';
 import { actions } from '../actions';
-import getInitialGame from '../../ai/getInitialGame';
+import getInitialGame, {initialGame} from '../../ai/getInitialGame';
 
-const game = (state, action) => {
+const game = (state=initialGame, action) => {
   switch (action.type) {
     case actions.NEW_GAME:
-      return aiStart(state);
+      // return aiStart(state);
+      aiStart();
+      return getInitialGame(state);
     case actions.SELECT_POSITION:
       return moveUserAndAi(state, action.index);
     case actions.ASK_AI_MOVE:
+      console.log('action-ASK_AI_MOVE');
       return askAiMove(state || getInitialGame());
     case actions.AI_MOVE:
       return moveAiAndNewGame(state || getInitialGame(), action.data);
     default:
-      return state || aiStart();
+      console.log('ai default in reducer');
+      return state; //|| aiStart();
   }
 };
 
