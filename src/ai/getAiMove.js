f16d6b104b884acb4c72b5501703cbcee44309b0
Grimmer <grimmer0125@gmail.com>
2018-04-16 01:32:09 +0800

    Change the target array used in net.propagate

--- src/ai/getAiMove.js
+++ src/ai/getAiMove.js
@@ -31,6 +31,7 @@ const learningRates = {
  * @return {void}
  */
 const propagate = (net, learningRate, game) => {
+  console.log('getBoardArray:', game);
   const tt = getBoardArray(game); //
   console.log('in propagate, 2nd arg, game target array(outputs):', tt); //[0,0,0,0,0,0,0,1,0]
   net.propagate(learningRate, tt); //(1 or 0.1, 0.6, ), 最新的已下的情形中, 修正的建議落子
@@ -38,6 +39,20 @@ const propagate = (net, learningRate, game) => {
 // 可查a. 之前查的 q-learning, b. alphago. c. 另一個tic-ai, c. enhacne/refornacne training
 // rnn
 // 問題 1. 重複的move,
+
+const propagate2 = (net, learningRate, index) => {
+  console.log('propagate2, target index:', index);
+  // const tt = getBoardArray(game); //
+  // console.log('in propagate, 2nd arg, game target array(outputs):', tt); //[0,0,0,0,0,0,0,1,0]
+  const predict = Array(9).fill(0);
+  predict[index]=1;
+  console.log('predict:', predict);
+  net.propagate(learningRate, predict); //(1 or 0.1, 0.6, ), 最新的已下的情形中, 修正的建議落子
+};
+
+export const startTrain = () => {
+  console.log('start Train');
+};
 
 /**
  * Get ai move index position
@@ -58,13 +73,13 @@ const getAiMove = (oldGame) => { //askaimove, 18, 9, 9
 
   console.log('input:', input, ';output:', output);
 
-  const index = getPositionIndex(output);
+  const index = getPositionIndex(output); //找最大值所在的index
   console.log('output from network predict: ai index:', index);
 
   const newGame = move(oldGame, index);
 
   if (newGame && newGame.ended) {
-    propagate(net, learningRates.win, newGame);
+    propagate2(net, learningRates.win, index);//newGame);
     console.log('train for game ended, ai index:', index);
     return index;
 
@@ -74,7 +89,7 @@ const getAiMove = (oldGame) => { //askaimove, 18, 9, 9
 
     console.log('best Positions:', bestPositions);//可以下的地方
     if (any(p => index === p, bestPositions)) {
-      propagate(net, learningRates.validMove, newGame);
+      propagate2(net, learningRates.validMove, index);//newGame);
       console.log('train for game, valid move, return ai index:', index);
 
       return index;
@@ -83,7 +98,7 @@ const getAiMove = (oldGame) => { //askaimove, 18, 9, 9
       const bestPosition = getRandomItem(bestPositions);
       const gameAfterBestMove = move(oldGame, bestPosition);
       console.log('get newgame by using RandomItem:', bestPosition); //ai先, 我, ai這次就跑到這. 因為不能下上次的位置
-      propagate(net, learningRates.invalidMove, gameAfterBestMove);
+      propagate2(net, learningRates.invalidMove, bestPosition);//gameAfterBestMove);
 
       console.log('train for game, invalid move, return randomPosition:', bestPosition);
 
