f318f24a0ea0c0c1bfd2db423982401dbb54c36c
Grimmer <grimmer0125@gmail.com>
2018-04-16 11:45:32 +0800

    Avoid invalid positions from network output (WIP)

--- src/ai/getPositionIndex.js
+++ src/ai/getPositionIndex.js
@@ -1,8 +1,17 @@
-const getPositionIndex = (aiOutput) => {
-  return aiOutput.reduce((bestIndex, value, index) =>
-    value > aiOutput[bestIndex]
-      ? index
-      : bestIndex, 0);
+const getPositionIndex = (board, aiOutput) => {
+  console.log('board:', board);
+  let bestIndex = -1;
+  const total = aiOutput.length;
+  for (let i=0; i< total;i++) {
+    if (board[i] ===0) {
+      if (bestIndex === -1){
+        bestIndex = i;
+      } else if ( aiOutput[i] > aiOutput[bestIndex]) {
+        bestIndex = i;
+      }
+    }
+  }
+  return bestIndex >-1?bestIndex:0;
 };
 
 export default getPositionIndex;
